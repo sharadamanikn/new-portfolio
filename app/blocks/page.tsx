a25a7b0ba522fc8b2febbf4b1664bba393c0b09e
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-17 18:18:38 +0530

    Add Blockpage component to render About and Technology sections

---
+++ app/blocks/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import About from './aboutblock/about'
+import Technology from './technologies/technologies'
+
+ const Blockpage = () => {
+  return (
+    <>
+    <About />
+    <Technology /></>
+  );
+};
+export default Blockpage;
