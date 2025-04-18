a2407e4cee49dacd2aece1ba9e918a096edbacb4
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 13:18:25 +0530

    Add Projects and Footer components to Blockpage for enhanced content display

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import About from './aboutblock/about'
 import Technology from './technologies/technologies'
+import Projects from './projectsblock/projects';
+import Footer from '../../components/Footer/footer';
 
  const Blockpage = () => {
   return (
     <>
     <About />
-    <Technology /></>
+    <Technology />
+    <Projects />
+    
+    </>
   );
 };
 export default Blockpage;
