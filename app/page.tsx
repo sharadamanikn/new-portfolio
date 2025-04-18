f6cf2f9c844da323b73c9f26c1f7e02668e3e50c
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 16:01:55 +0530

    Fix import statement for Projects component in RootPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import About from './blocks/aboutblock/about';
 import  Technology from './blocks/technologies/technologies';
+import Projects from './blocks/projectsblock/projects';
 const RootPage = () => {
   return (
     <div className='bg-gray-950'>
       <About />
       <Technology />
-      
+      <Projects />
     </div>
   );
 };
