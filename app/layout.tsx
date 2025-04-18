0af07e0c0aa075806d8f9a482f940cbb27530551
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 15:34:35 +0530

    Remove unused Open Graph metadata and clean up layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,24 +3,8 @@ import "./globals.css";
 import React, { PropsWithChildren } from 'react'
 import NavigationBar from "@/components/NavigationBar/NavBar";
 import Footer from "@/components/Footer/footer";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Portfolio- Sharadamani K N",
-  description: "Personal Portfolio Website.",
-  openGraph: {
-    title: "Portfolio",
-    description: "My Personal Portfolio Website.",
-    images: [
-      {
-        url: "https://myportfolio-blond-omega.vercel.app/og-card.png",
-        width: 1200,
-        height: 628,
-      },
-    ],
-  },
-};
-
+ 
 
  const RootLayout = (props:PropsWithChildren) => {
   return (
@@ -28,11 +12,14 @@ export const metadata: Metadata = {
       <head>
         <title className="text-5xl font-extrabold font-serif">{"SKN | Portfolio"}</title> 
       </head>
-    <body className="bg-gray-950">
+      
+    <body className="bg-gray-950 ">
+      <div>
       <NavigationBar />
       {props.children}
       <Footer />
       
+      </div>
     </body>
   </html>
   );
