b2876b395d948c15eef512519735058e15fd5f5a
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 15:34:51 +0530

    Refactor Technology component for improved layout and spacing consistency

--- app/blocks/technologies/technologies.tsx
+++ app/blocks/technologies/technologies.tsx
@@ -29,16 +29,16 @@ const technologies = [
 export default function Technology() {
   return (
     <section className="bg-gray-950 text-white py-12 px-4 ">
-      <div className="w-[90%] md:w-1/2 mx-auto">
-        <h1 className="text-4xl md:text-3xl mb-10 text-left">
+      <div className="max-w-3xl mx-auto px-4 ">
+        <h1 className="text-3xl md:text-2xl mb-10 text-left">
           <span className="text-white-400">Tech Stack </span>
         </h1>
 
-        <div className="flex flex-wrap justify-center md:justify-start gap-8">
+        <div className="flex flex-wrap justify-center md:justify-start gap-7">
           {technologies.map((tech) => (
             <div
               key={tech.name}
-              className="flex flex-col items-center justify-center w-[170px] h-[120px] border border-white-200 rounded-lg hover:shadow-[0_0_15px_rgba(59,130,246,0.7)] transition"
+              className="flex flex-col items-center justify-center w-[120px] h-[110px] border border-white-200 rounded-lg hover:shadow-[0_0_15px_rgba(59,130,246,0.7)] transition"
             >
               <div className="mb-2">{tech.icon}</div>
               <span className="text-sm font-mono text-center">{tech.name}</span>
