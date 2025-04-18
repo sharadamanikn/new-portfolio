11290945e791f8057c0d911dbc53b2dbabe0ff15
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 14:13:09 +0530

    Remove unused Footer component import from Blockpage

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import About from './aboutblock/about'
 import Technology from './technologies/technologies'
 import Projects from './projectsblock/projects';
-import Footer from '../../components/Footer/footer';
 
  const Blockpage = () => {
   return (
