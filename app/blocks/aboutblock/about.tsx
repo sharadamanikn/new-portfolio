fbf5e3323793d534095d0e30063b7c7d335da090
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 15:34:43 +0530

    Refactor About component for improved layout and typography consistency

--- app/blocks/aboutblock/about.tsx
+++ app/blocks/aboutblock/about.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 
 export const About = () => {
   return (
-    <div className="bg-gray-950 w-[90%] md:w-1/2 mx-auto mt-13 text-white">
-      <h1 className="text-4xl md:text-5xl font-extrabold leading-snug text-center">
+    <div className="bg-gray-950 max-w-3xl mx-auto px-4 mt-13 text-white">
+      <h1 className="text-2xl md:text-3xl font-extrabold leading-snug text-center">
         <span className="text-green-400">Hi, I&apos;m </span>
         <span className="text-sky-400">Sharadamani K N</span>
         <span className="ml-2"> 🚀</span>
       </h1>
 
-      <p className="text-1xl mt-2 text-neutral-300  text-center mb-3">
+      <p className="text-sm mt-2 text-neutral-300  text-center mb-3">
         Final Year Student   |   Information Science and Engineering
       </p>
       <div className="flex gap-6 text-sm text-gray-100 justify-center mb-8">
@@ -37,10 +37,10 @@ export const About = () => {
             <Instagram size={25} />
           </Link>
         </div>
-      <h1 className="text-4xl md:text-3xl leading-snug  mb-4">
+      <h1 className="text-3xl md:text-2xl leading-snug  mb-4">
         <span className="text-white-400 ">About Me </span>
       </h1> 
-            <p className="mt-4 text-neutral-200 leading-relaxed text-2xl text-justify mb-7">
+            <p className="mt-4 text-neutral-200 leading-relaxed text-1xl text-justify mb-7">
         I am currently pursuing my B.E. in Information Science and Engineering. I am a passionate learner and a tech enthusiast. I love to explore new technologies and learn them. 
         I am aspiring to pursue M.Tech in Computer Science to deepen my knowledge in the field. I love reading self improvement books and writing blogs about my learnings.
 
