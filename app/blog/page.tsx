d50f0ed52e087f77336f3979fe0c6f1793049e7d
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-18 15:55:29 +0530

    Fix heading size and adjust image dimensions for improved layout consistency

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ const Blog = () => {
 	return (
 		<div className="bg-gray-950 min-h-screen flex items-start justify-center pt-16">
 			<div className="w-[90%] md:w-1/2 px-6">
-				<h1 className="text-gray-400 text-2xl mb-6 text-center">
+				<h1 className="text-gray-400 text-1xl mb-6 text-center">
 					Just Me, Writing Stuff. ✍️
 				</h1>
 
@@ -12,7 +12,7 @@ const Blog = () => {
 				<div className="flex flex-col md:flex-row gap-6">
   <div className="flex-1 max-h-80">
     <Link href="https://sharadamanikn.blogspot.com/" target="_blank" passHref>
-      <div className="relative w-full h-60 md:h-72 cursor-pointer transition-transform duration-300 ease-in-out hover:shadow-[0_0_15px_rgba(59,130,246,0.7) hover:scale-90">
+      <div className="relative w-full h-50 md:h-70 cursor-pointer transition-transform duration-300 ease-in-out hover:shadow-[0_0_15px_rgba(59,130,246,0.7) hover:scale-90">
         <Image 
           src="/blog-image.png" 
           alt="Blog Image" 
@@ -25,8 +25,8 @@ const Blog = () => {
   </div>
 
   <div className="flex-1 flex flex-col justify-center border p-4 rounded-lg cursor-pointer transition-transform  shadow-lg border-gray-300  duration-300 ease-in-out hover:shadow-[0_0_15px_rgba(59,130,246,0.7)] hover:scale-90">
-    <h2 className="text-2xl md:text-4xl font-bold mb-3 text-white">Personal Blog</h2>
-    <p className="text-lg md:text-2xl font-light text-gray-300 mb-2">
+    <h2 className="text-2xl md:text-2xl font-bold mb-3 text-white">Personal Blog</h2>
+    <p className="text-lg md:text-1xl font-light text-gray-300 mb-2">
       A little corner where I pour out my thoughts, share life lessons. Don&apos;t worry — it&apos;s all non-technical stuff. ☕💭
     </p>
   </div>
